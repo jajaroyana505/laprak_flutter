997b4463e420f941010f100330e91d54d26258b6
USER 10 <jajaroyana505>
2023-06-12 16:50:51 +0700

    membuat login

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,32 @@
+// import 'package:flutter/material.dart';
+// import 'package:klinik_app_jajaroyana/ui/beranda.dart';
+// import '/ui/poli_page.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Klinik APP JAJA',
+//       debugShowCheckedModeBanner: false,
+//       home: Beranda(),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
-import 'package:klinik_app_jajaroyana/ui/beranda.dart';
-import '/ui/poli_page.dart';
+import 'package:klinik_app_jajaroyana/helper/user_info.dart';
+import '/ui/beranda.dart';
+import '/ui/login.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Klinik APP JAJA',
-      debugShowCheckedModeBanner: false,
-      home: Beranda(),
-    );
-  }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var token = await UserInfo().getToken();
+  print(token);
+  runApp(MaterialApp(
+    title: "Klinik APP",
+    debugShowCheckedModeBanner: false,
+    home: token == null ? Login() : Beranda(),
+  ));
 }
